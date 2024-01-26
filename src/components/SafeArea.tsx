263b2006a3762460fe9d76506447a3b605eca921
Doyun Yu <doyun0109@icloud.com>
2024-01-27 03:50:36 +0900

    Chore: Edit SafeArea

--- src/components/SafeArea.tsx
+++ src/components/SafeArea.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components/native';
 const SafeAreaView = styled.SafeAreaView`
   flex: 1;
   background-color: white;
+  align-items: flex-start;
 `;
 
 export default SafeAreaView;
