c57445b4cc19433c00f14ea3fd6508c3b560ec52
Doyun Yu <doyun0109@icloud.com>
2024-01-27 01:40:48 +0900

    Feat: ThemeProvider setting

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootNavigation} from '@navigation';
+import {useColorScheme} from 'react-native';
+import {dark, light} from '@theme';
+import {ThemeProvider} from 'styled-components';
 
 const App = () => {
+  const theme = useColorScheme();
+  const palette = theme === 'dark' ? dark : light;
   return (
-    <NavigationContainer>
-      <RootNavigation />
-    </NavigationContainer>
+    <ThemeProvider theme={palette}>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
--- declarations.d.ts
+++ declarations.d.ts
@@ -4,3 +4,10 @@ declare module '*.svg' {
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+import 'styled-components/native';
+import Palette from '@theme/types/Palette';
+
+declare module 'styled-components/native' {
+  export interface DefaultTheme extends Palette {}
+}
