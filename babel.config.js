23dcd02ab773f5d0c4d4eafb1ff6f8a6a94a142f
Doyun Yu <doyun0109@icloud.com>
2024-01-26 19:16:47 +0900

    Chore: Setting path alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@assets': './src/assets',
+          '@lib': './src/lib',
+          '@hooks': './src/hooks',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
