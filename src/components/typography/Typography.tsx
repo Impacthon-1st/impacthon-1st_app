1191d158a25ce62cba194a3816261afcd3fbe469
Doyun Yu <doyun0109@icloud.com>
2024-01-27 02:07:56 +0900

    Chore: 타이포그래피 높이 조정

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -8,24 +8,29 @@ const base = styled.Text<{$color: string}>`
 export const Title = styled(base)`
   font-weight: 700;
   font-size: 24px;
+  line-height: 30px;
 `;
 
 export const Headline = styled(base)`
   font-weight: 700;
   font-size: 20px;
+  line-height: 28px;
 `;
 
 export const Body = styled(base)<{$emphasized?: boolean}>`
   font-weight: ${props => (props.$emphasized ? 600 : 500)}
   font-size: 16px;
+    line-height: 26px;
 `;
 
 export const Content = styled(base)<{$emphasized?: boolean}>`
   font-weight: ${props => (props.$emphasized ? 600 : 500)}
   font-size: 14px;
+  line-height: 20px;
 `;
 
 export const Caption = styled(base)`
   font-weight: 500;
   font-size: 12px;
+  line-height: 18px;
 `;
