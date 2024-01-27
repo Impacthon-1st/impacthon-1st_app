0b5ad90047b0509a9f79e1adbd821c103d652232
Doyun Yu <doyun0109@icloud.com>
2024-01-27 19:34:29 +0900

    아이콘 수정

---
+++ src/components/icons/category/index.ts
@@ -0,0 +1,21 @@
+import CafeIcon from './CafeIcon.tsx';
+import DiningIcon from './DiningIcon.tsx';
+import FlightIcon from './FlightIcon.tsx';
+import PersonIcon from './PersonIcon.tsx';
+import PlanIcon from './PlanIcon.tsx';
+import RunIcon from './RunIcon.tsx';
+import TablewareIcon from './TablewareIcon.tsx';
+import TogetherIcon from './TogetherIcon.tsx';
+import TripIcon from './TripIcon.tsx';
+
+export {
+  CafeIcon,
+  DiningIcon,
+  FlightIcon,
+  PersonIcon,
+  PlanIcon,
+  RunIcon,
+  TablewareIcon,
+  TogetherIcon,
+  TripIcon,
+};
