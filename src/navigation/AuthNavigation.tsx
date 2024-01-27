4962cd007e1869f878fdd194c496f033c73b2952
Doyun Yu <doyun0109@icloud.com>
2024-01-27 19:35:28 +0900

    auth navigation

--- src/navigation/AuthNavigation.tsx
+++ src/navigation/AuthNavigation.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import TestScreen from '@screens/TestScreen';
+import {RegisterScreen} from '@screens/auth';
 
 const Stack = createStackNavigator();
 
 const AuthNavigation = () => {
   return (
     <Stack.Navigator
-      initialRouteName={'Login'}
+      initialRouteName={'Register'}
       screenOptions={{
         headerShown: false,
       }}>
       <Stack.Screen name="Login" component={TestScreen} />
-      <Stack.Screen name="Register" component={TestScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
   );
 };
--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import {BottomNavigation} from '@navigation';
+import {AuthNavigation, BottomNavigation} from '@navigation';
 import TestScreen from '@screens/TestScreen';
 
 const Stack = createStackNavigator();
@@ -15,6 +15,13 @@ const RootNavigation = () => {
       <Stack.Screen
         name={'Tab'}
         component={BottomNavigation}
+        options={{
+          gestureEnabled: false,
+        }}
+      />
+      <Stack.Screen
+        name={'Auth'}
+        component={AuthNavigation}
         options={{
           gestureEnabled: false,
         }}
