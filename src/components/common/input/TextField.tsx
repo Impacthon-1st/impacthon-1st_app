24736528b59302c4a8a7e19a2f765499c28e5d41
Doyun Yu <doyun0109@icloud.com>
2024-01-27 15:31:22 +0900

    Feat: Create TextField

---
+++ src/components/common/input/TextField.tsx
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import styled, {useTheme} from 'styled-components/native';
+import {InputModeOptions} from 'react-native';
+import Typography from '@components/typography';
+import {Row} from '@components/atomic';
+
+interface TextFieldProps {
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  description?: string;
+  value: string;
+  disabled?: boolean;
+  maxLength?: number;
+  multiline?: boolean;
+  height?: number;
+  inputMode?: InputModeOptions;
+  isSecure?: boolean;
+  onChange: (value: string) => void;
+}
+
+const TextField = ({
+  label,
+  required,
+  placeholder,
+  description,
+  value,
+  onChange,
+}: TextFieldProps) => {
+  const theme = useTheme();
+  const textStyles = Typography.Content.inlineStyle.generateStyleObject({
+    $color: theme.colors.gray.body,
+    theme: theme,
+  });
+  const [focused, setFocused] = useState(false);
+  return (
+    <Wrapper>
+      <Container>
+        {label && (
+          <Row $gap={2}>
+            <Typography.Content $color={theme.colors.gray.subBody} $emphasized>
+              {label}
+            </Typography.Content>
+            {required && <Required />}
+          </Row>
+        )}
+        <InputContainer
+          $focused={focused}
+          placeholder={placeholder}
+          placeholderTextColor={theme.colors.gray.disabled}
+          value={value}
+          onChangeText={onChange}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+        />
+        {description && (
+          <Typography.Caption $color={theme.colors.gray.disabled}>
+            {description}
+          </Typography.Caption>
+        )}
+      </Container>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.View`
+  width: 100%;
+  padding: 8px 20px;
+`;
+
+const Container = styled.View`
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+`;
+
+const InputContainer = styled.TextInput<{$focused: boolean}>`
+  border-radius: 12px;
+  background-color: ${({theme}) => theme.colors.gray.solid};
+  border: ${props =>
+    props.$focused ? `1px solid ${props.theme.colors.gray.subSolid}` : 'none'};
+  padding: 16px;
+  height: 54px;
+`;
+
+const Required = styled.View`
+  width: 4px;
+  height: 4px;
+  border-radius: 2px;
+  background-color: ${({theme}) => theme.colors.primary.default};
+`;
+
+export default TextField;
